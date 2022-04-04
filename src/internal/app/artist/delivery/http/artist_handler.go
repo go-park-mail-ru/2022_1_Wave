8db75a88d74e74433811266c6e925b1ecf4a6fce
mausved <binmaldo@mail.ru>
2022-04-04 22:21:49 +0300

    WV-60: реализация сущности "артист"

---
+++ src/internal/app/artist/delivery/http/artist_handler.go
@@ -0,0 +1,97 @@
+package artistDeliveryHttp
+
+import (
+	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/domain"
+	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/structs/delivery/http"
+	"net/http"
+)
+
+var Handler structsDeliveryHttp.Handler
+
+// GetAll godoc
+// @Summary      GetAll
+// @Description  getting all artists
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Success      200  {object}  webUtils.Success
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/ [get]
+func GetAll(w http.ResponseWriter, _ *http.Request) {
+	Handler.GetAll(w, domain.ArtistMutex)
+}
+
+// Create godoc
+// @Summary      Create
+// @Description  creating new artist
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Param        Artist  body      domain.Artist  true  "params of new artist. Id will be set automatically."
+// @Success      200     {object}  webUtils.Success
+// @Failure      400     {object}  webUtils.Error  "Data is invalid"
+// @Failure      405     {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/ [post]
+func Create(w http.ResponseWriter, r *http.Request) {
+	Handler.Create(w, r, domain.ArtistMutex)
+}
+
+// Update godoc
+// @Summary      Update
+// @Description  updating artist by id
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Param        Artist  body      domain.Artist  true  "id of updating artist and params of it."
+// @Success      200     {object}  webUtils.Success
+// @Failure      400     {object}  webUtils.Error  "Data is invalid"
+// @Failure      405     {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/ [put]
+func Update(w http.ResponseWriter, r *http.Request) {
+	Handler.Update(w, r, domain.ArtistMutex)
+}
+
+// Get godoc
+// @Summary      Get
+// @Description  getting artist by id
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Param        id   path      integer  true  "id of artist which need to be getted"
+// @Success      200  {object}  domain.Artist
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/{id} [get]
+func Get(w http.ResponseWriter, r *http.Request) {
+	Handler.Get(w, r, domain.ArtistMutex)
+}
+
+// Delete godoc
+// @Summary      Delete
+// @Description  deleting artist by id
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Param        id   path      integer  true  "id of artist which need to be deleted"
+// @Success      200  {object}  webUtils.Success
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/{id} [delete]
+func Delete(w http.ResponseWriter, r *http.Request) {
+	Handler.Delete(w, r, domain.ArtistMutex)
+}
+
+// GetPopular godoc
+// @Summary      GetPopular
+// @Description  getting top20 popular artists
+// @Tags         artist
+// @Accept          application/json
+// @Produce      application/json
+// @Success      200  {object}  webUtils.Success
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/artists/popular [get]
+func GetPopular(w http.ResponseWriter, _ *http.Request) {
+	Handler.GetPopular(w, domain.ArtistMutex)
+}
---
+++ src/internal/app/artist/usecase/artist_usecase.go
@@ -0,0 +1,7 @@
+package artistUseCase
+
+import (
+	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/structs/usecase"
+)
+
+var UseCase structsUseCase.UseCase
