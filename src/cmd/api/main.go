879ed366f7d6a778c3cbdf23e7b9e2781bfff235
mausved <binmaldo@mail.ru>
2022-04-05 18:35:32 +0300

    WV-63: запуск echo

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	_ "github.com/go-park-mail-ru/2022_1_Wave/docs"
 	"github.com/go-park-mail-ru/2022_1_Wave/init/router"
 	"github.com/go-park-mail-ru/2022_1_Wave/init/storage"
 	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/structs/interfaces"
 	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/structs/storage/local"
-	"log"
-	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"reflect"
 )
 
 // ConfigFilename config
@@ -14,6 +16,8 @@ const ConfigFilename = "config.toml"
 const port = ":5000"
 
 func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
 	//if err := config.LoadConfig(ConfigFilename); err != nil {
 	//	log.Fatal(err)
 	//} else {
@@ -22,14 +26,24 @@ func main() {
 
 	const quantity = 10
 	localStorage := utilsInterfaces.GlobalStorageInterface(structStorageLocal.LocalStorage{})
-	storage.InitStorage(quantity, &localStorage)
 
-	rout := router.Router()
-
-	log.Println("start serving :5000")
-
-	err := http.ListenAndServe(port, rout)
-	if err != nil {
-		log.Fatal(err)
+	if err := storage.InitStorage(quantity, &localStorage); err != nil {
+		e.Logger.Fatal("error to init storage type <%v>, err: %v\n", reflect.TypeOf(localStorage), err)
 	}
+
+	e.Logger.Printf("Success init local storage type <%v>\n", reflect.TypeOf(localStorage))
+	e.Logger.Printf("Artists: %v", localStorage.GetArtistRepoLen())
+	e.Logger.Printf("Albums: %v", localStorage.GetAlbumRepoLen())
+	e.Logger.Printf("Tracks: %v", localStorage.GetArtistRepoLen())
+
+	router.Router(e)
+
+	e.Logger.Warnf("start listening on %s", port)
+	err := e.Start("0.0.0.0:5000")
+	if err != nil {
+		e.Logger.Errorf("server error: %s", err)
+	}
+
+	e.Logger.Warnf("shutdown")
+
 }
