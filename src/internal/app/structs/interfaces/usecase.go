d8f3f576fc404cf936af0598671eedbc1e148772
mausved <binmaldo@mail.ru>
2022-04-05 18:27:52 +0300

    WV-63: добавление указателя для модели, так как сеттится айди автоматом

--- src/internal/app/structs/interfaces/usecase.go
+++ src/internal/app/structs/interfaces/usecase.go
@@ -8,7 +8,7 @@ import (
 type UseCaseInterface interface {
 	GetAll(mutex *sync.RWMutex) (*[]Domain, error)
 	GetLastId(mutex *sync.RWMutex) (id uint64, err error)
-	Create(Domain, *sync.RWMutex) (UseCaseInterface, error)
+	Create(*Domain, *sync.RWMutex) (UseCaseInterface, error)
 	Update(Domain, *sync.RWMutex) (UseCaseInterface, error)
 	Delete(id uint64, mutex *sync.RWMutex) (UseCaseInterface, error)
 	GetById(id uint64, mutex *sync.RWMutex) (*Domain, error)
