74bd39d37a47c7fadbcb206a694460b9512f0df5
mausved <binmaldo@mail.ru>
2022-04-08 18:31:13 +0300

    WV-59: добавление констант для ограничений

--- src/internal/constants.go
+++ src/internal/constants.go
@@ -52,6 +52,12 @@ const (
 	AlbumTitleLen = varchar255
 )
 
+// album cover constraints
+const (
+	AlbumCoverTitleLen = varchar255
+	AlbumCoverQuoteLen = varchar512
+)
+
 // track constraints
 const (
 	TrackTitleLen   = varchar255
@@ -76,11 +82,18 @@ const (
 	ErrorAlbumCoverIdIsNegative        = "cover's id is negative"
 )
 
+// album cover errors
+const (
+	ErrorAlbumCoverMaxTitleLen = "album's cover title length is over than max"
+	ErrorAlbumCoverMaxQuoteLen = "album's cover quote length is over than max"
+)
+
 // tracks errors
 const (
 	ErrorTrackIdIsNegative             = "track's id is negative"
 	ErrorTrackMaxTitleLen              = "track's title length is over than max"
 	ErrorTrackArtistIdIsNegative       = "author's id of track is negative"
+	ErrorTrackAlbumIdIsNegative        = "author's id of track is negative"
 	ErrorTrackCoverIdIsNegative        = "cover's id of track is negative"
 	ErrorTrackMaxPhotoLinkLen          = "length of song's link to source is over than max"
 	ErrorTrackMp4MaxLinkLen            = "length of Mp4's link to source is over than max"
@@ -99,16 +112,35 @@ const (
 	ErrorArtistCountListeningIsNegative = "number of artist's listening is negative"
 )
 
+// db types
+const (
+	Album      = "album"
+	AlbumCover = "albumCover"
+	Artist     = "artist"
+	Track      = "track"
+)
+
+// db postgres fields
+const (
+	Count_listening = "count_listening"
+	Count_followers = "count_followers"
+)
+
+// db postgres order
+const Desc = "DESC"
+
 // db errors
 const (
-	ErrorDbIsEmpty = "database is empty"
+	ErrorDbIsEmpty       = "database is empty"
+	ErrorNothingToUpdate = "nothing to update (check id)"
 )
 
 // local db errors
 const (
-	ErrorLocalDbArtistsNotEnought = "sum of artists not equal to input quantity"
-	ErrorLocalDbAlbumsNotEnought  = "sum of albums not equal to input quantity"
-	ErrorLocalDbTracksNotEnought  = "sum of tracks not equal to input quantity"
+	ErrorLocalDbArtistsNotEnought     = "sum of artists not equal to input quantity"
+	ErrorLocalDbAlbumsNotEnought      = "sum of albums not equal to input quantity"
+	ErrorLocalDbAlbumCoversNotEnought = "sum of covers of albums not equal to input quantity"
+	ErrorLocalDbTracksNotEnought      = "sum of tracks not equal to input quantity"
 )
 
 // album fields
@@ -116,10 +148,12 @@ const (
 	FieldId             = "id"
 	FieldTitle          = "title"
 	FieldArtist         = "artist"
-	FieldAuthorId       = "authorId"
+	FieldArtistId       = "artistId"
 	FieldAlbumId        = "albumId"
 	FieldCover          = "cover"
 	FieldCountLikes     = "countLikes"
+	FieldLikes          = "likes"
+	FieldListenings     = "listenings"
 	FieldCountListening = "countListening"
 	FieldDate           = "date"
 	FieldCoverId        = "coverId"
@@ -128,6 +162,9 @@ const (
 	FieldCountFollowers = "countFollowers"
 	FieldDuration       = "duration"
 	FieldMp4            = "mp4"
+	FieldSrc            = "src"
+	FieldIsDark         = "isDark"
+	FieldQuote          = "quote"
 )
 
 // others
@@ -136,8 +173,14 @@ const (
 	AlbumPreName  = "album_"
 	ArtistPreName = "artist_"
 	TrackPreName  = "track_"
-	PngFormat     = ".png"
 	NullId        = 0
 	BadId         = -1
 	BadType       = "bad type"
+	Top           = 20
+)
+
+// formats
+const (
+	PngFormat = ".png"
+	Mp4Format = ".mp4"
 )
