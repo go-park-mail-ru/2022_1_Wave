96b984404f68d73092f77c0ca64ab35071db34ca
mausved <binmaldo@mail.ru>
2022-04-08 18:36:54 +0300

    WV-59: компоненты для сущности "обложка альбома"

---
+++ src/internal/app/albumCover/delivery/http/albumCover_handler.go
@@ -0,0 +1,89 @@
+package albumCoverDeliveryHttp
+
+import (
+	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/domain"
+	"github.com/go-park-mail-ru/2022_1_Wave/internal/app/structs/delivery/http"
+	"github.com/labstack/echo/v4"
+)
+
+var Handler structsDeliveryHttp.Handler
+
+// GetAll godoc
+// @Summary      GetAll
+// @Description  getting all albums cover
+// @Tags         albumCover
+// @Accept          application/json
+// @Produce      application/json
+// @Success      200  {object}  webUtils.Success
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/albumCovers/ [get]
+func GetAll(ctx echo.Context) error {
+	return Handler.GetAll(ctx, domain.AlbumCoverMutex)
+}
+
+// Create godoc
+// @Summary      Create
+// @Description  creating new albumCover
+// @Tags         albumCover
+// @Accept       application/json
+// @Produce      application/json
+// @Param        Album  body      domain.AlbumCover  true  "params of new album cover. Id will be set automatically."
+// @Success      200    {object}  webUtils.Success
+// @Failure      400    {object}  webUtils.Error  "Data is invalid"
+// @Failure      405    {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/albumCovers/ [post]
+func Create(ctx echo.Context) error {
+	proxy, err := Handler.Create(ctx, domain.AlbumCoverMutex)
+	Handler = proxy.(structsDeliveryHttp.Handler)
+	return err
+}
+
+// Update godoc
+// @Summary      Update
+// @Description  updating album cover by id
+// @Tags         albumCover
+// @Accept          application/json
+// @Produce      application/json
+// @Param        AlbumCover  body      domain.AlbumCover  true  "id of updating album cover and params of it."
+// @Success      200    {object}  webUtils.Success
+// @Failure      400    {object}  webUtils.Error  "Data is invalid"
+// @Failure      405    {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/albumCovers/ [put]
+func Update(ctx echo.Context) error {
+	proxy, err := Handler.Update(ctx, domain.AlbumCoverMutex)
+	Handler = proxy.(structsDeliveryHttp.Handler)
+	return err
+}
+
+// Get godoc
+// @Summary      Get
+// @Description  getting album cover by id
+// @Tags         albumCover
+// @Accept       application/json
+// @Produce      application/json
+// @Param        id   path      integer  true  "id of album cover which need to be getted"
+// @Success      200  {object}  domain.AlbumCover
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/albumCovers/{id} [get]
+func Get(ctx echo.Context) error {
+	return Handler.Get(ctx, domain.AlbumCoverMutex)
+}
+
+// Delete godoc
+// @Summary      Delete
+// @Description  deleting album cover by id
+// @Tags         albumCover
+// @Accept          application/json
+// @Produce      application/json
+// @Param        id   path      integer  true  "id of album cover which need to be deleted"
+// @Success      200  {object}  webUtils.Success
+// @Failure      400  {object}  webUtils.Error  "Data is invalid"
+// @Failure      405  {object}  webUtils.Error  "Method is not allowed"
+// @Router       /api/v1/albumCovers/{id} [delete]
+func Delete(ctx echo.Context) error {
+	proxy, err := Handler.Delete(ctx, domain.AlbumCoverMutex)
+	Handler = proxy.(structsDeliveryHttp.Handler)
+	return err
+}
