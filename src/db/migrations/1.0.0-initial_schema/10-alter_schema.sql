b12c38ecbcfa3e73f9403ba3ba72aa3781d5a0a2
mausved <binmaldo@mail.ru>
2022-04-08 18:37:26 +0300

    WV-59: перепись миграции базы

--- src/db/migrations/1.0.0-initial_schema/10-alter_schema.sql
+++ src/db/migrations/1.0.0-initial_schema/10-alter_schema.sql
@@ -1,24 +1,46 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.1
--- Dumped by pg_dump version 14.1
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-SET default_tablespace = '';
-SET default_with_oids = false;
-SET default_table_access_method = heap;
-SET search_path = public, pg_catalog;
+SET
+check_function_bodies = false;
+SET
+xmloption = content;
+SET
+client_min_messages = warning;
+SET
+row_security = off;
+SET
+default_tablespace = '';
+SET
+default_with_oids = false;
+SET
+default_table_access_method = heap;
+SET
+search_path = public, pg_catalog;
+
+
+
+DROP TABLE if exists UserAlbumsLike;
+DROP TABLE if exists UserArtistsFollowing;
+DROP TABLE if exists UserTracksLike;
+DROP TABLE if exists UserListenedTrack;
+DROP TABLE if exists UserPlaylist;
+DROP TABLE if exists UserListenedPlaylist;
+DROP TABLE if exists UserPlayer;
+DROP TABLE if exists PlaylistTrack;
+DROP TABLE if exists Playlist;
+DROP TABLE if exists Users;
+DROP TABLE if exists Track;
+DROP TABLE if exists Album;
+DROP TABLE if exists Single;
+DROP TABLE if exists AlbumCover;
+DROP TABLE if exists Artist;
+DROP TABLE if exists place;
+
+
 
 CREATE TABLE Users
 (
@@ -35,30 +57,31 @@ CREATE TABLE Users
 
 
 
-CREATE TABLE Tracks
+CREATE TABLE Track
 (
     id              serial       NOT NULL,
-    album_id        integer      NOT NULL,
-    author_id       integer      NOT NULL,
+    album_id        integer,
+    artist_id       integer      NOT NULL,
+--     cover_id        serial       NOT NULL,
     title           varchar(255) NOT NULL,
     duration        integer      NOT NULL,
-    mp4             varchar(255) NOT NULL,
+--     mp4_id          serial       NOT NULL,
     count_likes     integer      NOT NULL,
     count_listening integer      NOT NULL,
-    CONSTRAINT Tracks_pk PRIMARY KEY (id)
+    CONSTRAINT Track_pk PRIMARY KEY (id)
 ) WITH (
       OIDS= FALSE
     );
 
 
 
-CREATE TABLE UserTracksLikes
+CREATE TABLE UserTracksLike
 (
     user_id  integer NOT NULL,
     track_id integer NOT NULL
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
@@ -72,14 +95,14 @@ CREATE TABLE UserArtistsFollowing
 
 
 
-CREATE TABLE UserListenedTracks
+CREATE TABLE UserListenedTrack
 (
-    user_id        integer  NOT NULL,
-    track_id       integer  NOT NULL,
-    listening_date DATE NOT NULL DEFAULT 'now'
+    user_id        integer NOT NULL,
+    track_id       integer NOT NULL,
+    listening_date DATE    NOT NULL DEFAULT 'now'
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
@@ -97,74 +120,83 @@ CREATE TABLE UserPlayer
 
 
 
-CREATE TABLE Playlists
+CREATE TABLE Playlist
 (
     id              serial       NOT NULL,
     title           varchar(255) NOT NULL,
-    author_id       integer      NOT NULL,
-    date_create     DATE     NOT NULL DEFAULT 'now',
+    artist_id       integer      NOT NULL,
+    date_create     DATE         NOT NULL DEFAULT 'now',
     count_likes     integer      NOT NULL,
     count_added     integer      NOT NULL,
     count_listening integer      NOT NULL,
-    CONSTRAINT Playlists_pk PRIMARY KEY (id)
+    CONSTRAINT Playlist_pk PRIMARY KEY (id)
 ) WITH (
       OIDS= FALSE
     );
 
 
 
-CREATE TABLE PlaylistTracks
+CREATE TABLE PlaylistTrack
 (
     playlist_id integer NOT NULL,
     track_id    integer NOT NULL
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
-CREATE TABLE UserPlaylists
+CREATE TABLE UserPlaylist
 (
     user_id     integer NOT NULL,
     playlist_id integer NOT NULL
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
-CREATE TABLE UserListenedPlaylists
+CREATE TABLE UserListenedPlaylist
 (
-    user_id        integer  NOT NULL,
-    playlist_id    integer  NOT NULL,
-    date_listening DATE NOT NULL
+    user_id        integer NOT NULL,
+    playlist_id    integer NOT NULL,
+    date_listening DATE    NOT NULL
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
-CREATE TABLE Albums
+CREATE TABLE Album
 (
     id              serial       NOT NULL,
     title           varchar(255) NOT NULL,
-    author_id       integer      NOT NULL,
+    artist_id       integer      NOT NULL,
     count_likes     integer      NOT NULL,
     count_listening integer      NOT NULL,
-    date            DATE     NOT NULL,
-    cover_id        integer      NOT NULL UNIQUE,
-    CONSTRAINT Albums_pk PRIMARY KEY (id)
+    date            bigint       NOT NULL,
+--     cover_id        serial       NOT NULL,
+    CONSTRAINT Album_pk PRIMARY KEY (id)
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
+-- CREATE TABLE Single
+-- (
+--     id         serial  NOT NULL,
+--     artist_id  integer NOT NULL UNIQUE,
+--     singles_id integer NOT NULL,
+--     CONSTRAINT Single_pk PRIMARY KEY (id)
+-- ) WITH (
+--       OIDS= FALSE
+--     );
 
 
 CREATE TABLE Artist
 (
     id              serial       NOT NULL,
     name            varchar(255) NOT NULL,
-    photo           varchar(255) NOT NULL,
+--     photo_id        serial      NOT NULL,
     count_followers integer      NOT NULL,
     count_listening integer      NOT NULL,
     CONSTRAINT Artist_pk PRIMARY KEY (id)
@@ -174,99 +206,90 @@ CREATE TABLE Artist
 
 
 
-CREATE TABLE AlbumCovers
+CREATE TABLE AlbumCover
 (
     id      serial       NOT NULL,
     title   varchar(255) NOT NULL,
-    photo   varchar(255) NOT NULL,
     quote   varchar(512) NOT NULL,
     is_dark BOOLEAN      NOT NULL,
-    CONSTRAINT AlbumCovers_pk PRIMARY KEY (id)
+    CONSTRAINT AlbumCover_pk PRIMARY KEY (id)
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
-
-CREATE TABLE UserAlbumsLikes
+CREATE TABLE UserAlbumsLike
 (
     user_id  integer NOT NULL,
     album_id integer NOT NULL
 ) WITH (
-      OIDS= FALSE
-    );
+      OIDS = FALSE
+      );
 
 
 
-ALTER TABLE Tracks
-    ADD CONSTRAINT Tracks_fk0 FOREIGN KEY (album_id) REFERENCES Albums (id);
-ALTER TABLE Tracks
-    ADD CONSTRAINT Tracks_fk1 FOREIGN KEY (author_id) REFERENCES Artist (id);
+-- ALTER TABLE Track
+--     ADD CONSTRAINT Tracks_fk0 FOREIGN KEY (album_id) REFERENCES Album (id);
+ALTER TABLE Track
+    ADD CONSTRAINT Tracks_fk1 FOREIGN KEY (artist_id) REFERENCES Artist (id);
 
-ALTER TABLE UserTracksLikes
-    ADD CONSTRAINT UserTracksLikes_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
-ALTER TABLE UserTracksLikes
-    ADD CONSTRAINT UserTracksLikes_fk1 FOREIGN KEY (track_id) REFERENCES Tracks (id);
+ALTER TABLE UserTracksLike
+    ADD CONSTRAINT UserTracksLike_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE UserTracksLike
+    ADD CONSTRAINT UserTracksLike_fk1 FOREIGN KEY (track_id) REFERENCES Track (id);
 
 ALTER TABLE UserArtistsFollowing
     ADD CONSTRAINT UserArtistsFollowing_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
 ALTER TABLE UserArtistsFollowing
     ADD CONSTRAINT UserArtistsFollowing_fk1 FOREIGN KEY (artist_id) REFERENCES Artist (id);
 
-ALTER TABLE UserListenedTracks
-    ADD CONSTRAINT UserListenedTracks_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
-ALTER TABLE UserListenedTracks
-    ADD CONSTRAINT UserListenedTracks_fk1 FOREIGN KEY (track_id) REFERENCES Tracks (id);
+ALTER TABLE UserListenedTrack
+    ADD CONSTRAINT UserListenedTrack_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE UserListenedTrack
+    ADD CONSTRAINT UserListenedTrack_fk1 FOREIGN KEY (track_id) REFERENCES Track (id);
 
 ALTER TABLE UserPlayer
     ADD CONSTRAINT UserPlayer_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
 ALTER TABLE UserPlayer
-    ADD CONSTRAINT UserPlayer_fk1 FOREIGN KEY (track_id) REFERENCES Tracks (id);
+    ADD CONSTRAINT UserPlayer_fk1 FOREIGN KEY (track_id) REFERENCES Track (id);
 ALTER TABLE UserPlayer
-    ADD CONSTRAINT UserPlayer_fk2 FOREIGN KEY (playlist_id) REFERENCES Tracks (id);
+    ADD CONSTRAINT UserPlayer_fk2 FOREIGN KEY (playlist_id) REFERENCES Track (id);
 ALTER TABLE UserPlayer
-    ADD CONSTRAINT UserPlayer_fk3 FOREIGN KEY (album_id) REFERENCES Tracks (id);
+    ADD CONSTRAINT UserPlayer_fk3 FOREIGN KEY (album_id) REFERENCES Track (id);
 
-ALTER TABLE Playlists
-    ADD CONSTRAINT Playlists_fk0 FOREIGN KEY (author_id) REFERENCES Users (id);
+ALTER TABLE Playlist
+    ADD CONSTRAINT Playlist_fk0 FOREIGN KEY (artist_id) REFERENCES Users (id);
 
-ALTER TABLE PlaylistTracks
-    ADD CONSTRAINT PlaylistTracks_fk0 FOREIGN KEY (playlist_id) REFERENCES Playlists (id);
-ALTER TABLE PlaylistTracks
-    ADD CONSTRAINT PlaylistTracks_fk1 FOREIGN KEY (track_id) REFERENCES Tracks (id);
+ALTER TABLE PlaylistTrack
+    ADD CONSTRAINT PlaylistTrack_fk0 FOREIGN KEY (playlist_id) REFERENCES Playlist (id);
+ALTER TABLE PlaylistTrack
+    ADD CONSTRAINT PlaylistTrack_fk1 FOREIGN KEY (track_id) REFERENCES Track (id);
 
-ALTER TABLE UserPlaylists
-    ADD CONSTRAINT UserPlaylists_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
-ALTER TABLE UserPlaylists
-    ADD CONSTRAINT UserPlaylists_fk1 FOREIGN KEY (playlist_id) REFERENCES Playlists (id);
+ALTER TABLE UserPlaylist
+    ADD CONSTRAINT UserPlaylist_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE UserPlaylist
+    ADD CONSTRAINT UserPlaylist_fk1 FOREIGN KEY (playlist_id) REFERENCES Playlist (id);
 
-ALTER TABLE UserListenedPlaylists
-    ADD CONSTRAINT UserListenedPlaylists_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
-ALTER TABLE UserListenedPlaylists
-    ADD CONSTRAINT UserListenedPlaylists_fk1 FOREIGN KEY (playlist_id) REFERENCES Playlists (id);
+ALTER TABLE UserListenedPlaylist
+    ADD CONSTRAINT UserListenedPlaylist_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE UserListenedPlaylist
+    ADD CONSTRAINT UserListenedPlaylist_fk1 FOREIGN KEY (playlist_id) REFERENCES Playlist (id);
 
-ALTER TABLE Albums
-    ADD CONSTRAINT Albums_fk0 FOREIGN KEY (author_id) REFERENCES Artist (id);
-ALTER TABLE Albums
-    ADD CONSTRAINT Albums_fk1 FOREIGN KEY (cover_id) REFERENCES AlbumCovers (id);
+ALTER TABLE Album
+    ADD CONSTRAINT Album_fk0 FOREIGN KEY (artist_id) REFERENCES Artist (id);
+ALTER TABLE Album
+    ADD CONSTRAINT Album_fk1 FOREIGN KEY (id) REFERENCES AlbumCover (id);
 
+ALTER TABLE UserAlbumsLike
+    ADD CONSTRAINT UserAlbumsLike_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE UserAlbumsLike
+    ADD CONSTRAINT UserAlbumsLike_fk1 FOREIGN KEY (album_id) REFERENCES Album (id);
 
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
 
-ALTER TABLE UserAlbumsLikes
-    ADD CONSTRAINT UserAlbumsLikes_fk0 FOREIGN KEY (user_id) REFERENCES Users (id);
-ALTER TABLE UserAlbumsLikes
-    ADD CONSTRAINT UserAlbumsLikes_fk1 FOREIGN KEY (album_id) REFERENCES Albums (id);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+select id
+from track
+order by id;
