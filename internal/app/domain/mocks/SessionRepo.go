ca39d1e2026cfd9126fd9b2a3d401cdc7bb70ed8
mausved <binmaldo@mail.ru>
2022-04-14 16:17:35 +0300

    WV-64: генерация метода размера таблицы в моках

--- internal/app/domain/mocks/SessionRepo.go
+++ internal/app/domain/mocks/SessionRepo.go
@@ -51,6 +51,27 @@ func (_m *SessionRepo) GetSession(sessionId string) (*domain.Session, error) {
 	return r0, r1
 }
 
+// GetSize provides a mock function with given fields:
+func (_m *SessionRepo) GetSize() (int, error) {
+	ret := _m.Called()
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func() int); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // MakeSessionAuthorized provides a mock function with given fields: sessionId, userId
 func (_m *SessionRepo) MakeSessionAuthorized(sessionId string, userId uint) error {
 	ret := _m.Called(sessionId, userId)
--- internal/app/domain/mocks/UserRepo.go
+++ internal/app/domain/mocks/UserRepo.go
@@ -26,6 +26,27 @@ func (_m *UserRepo) Delete(id uint) error {
 	return r0
 }
 
+// GetSize provides a mock function with given fields:
+func (_m *UserRepo) GetSize() (int, error) {
+	ret := _m.Called()
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func() int); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Insert provides a mock function with given fields: user
 func (_m *UserRepo) Insert(user *domain.User) error {
 	ret := _m.Called(user)
