4d003ec62f03f55b262af8f39785b54fef1d4832
mausved <binmaldo@mail.ru>
2022-04-28 20:30:12 +0300

    WV-71: корректировка прода

---
+++ dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+go get -u google.golang.org/protobuf
+go get -u google.golang.org/grpc
+go get -u golang.org/x/net/context
+bash -c "mkdir -p tmp && go build -o ./tmp/wave.music cmd/api/main.go && ./tmp/wave.music"
+bash reload.sh
