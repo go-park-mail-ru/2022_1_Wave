2592702fc3367eb2b1db8874f23242535a42ed5b
mausved <binmaldo@mail.ru>
2022-05-02 20:42:54 +0300

    WV-71: восстановление коммитов

--- prod.sh
+++ prod.sh
@@ -2,5 +2,5 @@
 go get -u google.golang.org/protobuf
 go get -u google.golang.org/grpc
 go get -u golang.org/x/net/context
-bash -c "mkdir -p tmp && go build -o ./tmp/wave.music cmd/api/main.go && ./tmp/wave.music"
 bash reload.sh
+bash -c "mkdir -p tmp && go build -o ./tmp/wave.music cmd/api/main.go && ./tmp/wave.music"
