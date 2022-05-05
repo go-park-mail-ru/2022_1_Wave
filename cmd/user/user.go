52619872a8b28837e448688f75621af50762c497
mausved <binmaldo@mail.ru>
2022-05-05 14:22:37 +0300

    WV-73: подгрузка изменений с WV-71

--- cmd/user/user.go
+++ cmd/user/user.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-	InitDb "github.com/go-park-mail-ru/2022_1_Wave/init/db"
+	"database/sql"
 	"github.com/go-park-mail-ru/2022_1_Wave/init/logger"
 	"github.com/go-park-mail-ru/2022_1_Wave/internal/microservices/user/proto"
 	"github.com/go-park-mail-ru/2022_1_Wave/internal/microservices/user/repository/postgresql"
 	user_service "github.com/go-park-mail-ru/2022_1_Wave/internal/microservices/user/service"
-	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
 	"google.golang.org/grpc"
 	"net"
 	"os"
 )
 
-func main() {
-	sqlxDb, err := InitDb.InitDatabase("DATABASE_CONNECTION")
-	if err != nil {
-		os.Exit(1)
+func InitDatabase() *sqlx.DB {
+	dsn := os.Getenv("DATABASE_CONNECTION")
+	if dsn == "" {
+		dsn = "user=test dbname=test password=test host=localhost port=5500 sslmode=disable"
 	}
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil
+	}
+	err = db.Ping() // вот тут будет первое подключение к базе
+	if err != nil {
+		return nil
+	}
+
+	db.SetMaxOpenConns(10)
+
+	sqlxDb := sqlx.NewDb(db, "pgx")
+	_ = sqlxDb.Ping()
+
+	return sqlxDb
+}
+
+func main() {
+	sqlxDb := InitDatabase()
 	userRepo := postgresql.NewUserPostgresRepo(sqlxDb)
 
 	defer func() {
@@ -25,7 +44,7 @@ func main() {
 		}
 	}()
 
-	port := ":8086"
+	port := os.Getenv("USER_PORT")
 	listen, err := net.Listen("tcp", port)
 	if err != nil {
 		logger.GlobalLogger.Logrus.Errorf("error listen on %s port: %s", port, err.Error())
@@ -33,7 +52,7 @@ func main() {
 
 	server := grpc.NewServer()
 	proto.RegisterProfileServer(server, user_service.NewUserService(userRepo))
-	//logger.GlobalLogger.Logrus.Printf("started profile microservice on %s", port)
+	logger.GlobalLogger.Logrus.Printf("started profile microservice on %s", port)
 	err = server.Serve(listen)
 	if err != nil {
 		logger.GlobalLogger.Logrus.Errorf("cannot listen port %s: %s", port, err.Error())
