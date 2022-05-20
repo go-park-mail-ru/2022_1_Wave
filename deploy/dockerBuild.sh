23de4d5e967f278b3d530fa34730470654f227ab
mausved <binmaldo@mail.ru>
2022-05-20 22:48:50 +0300

    WV-76: fix demon

--- deploy/dockerBuild.sh
+++ deploy/dockerBuild.sh
@@ -9,12 +9,13 @@ cd ..
 echo -e "\033[32m *** BUILDING $dockerProfile/$prefix$1 ***\033[36m"
 docker build -q -t "$dockerProfile/$prefix$1" -f env/prod/k8/$1/Dockerfile .
 
+
 #spin
 
 if [ $? -eq 0 ]; then
-  echo -e "\033[32m *** SUCCESS BUILD mausved/wave_$1 ***"
+  echo -e "\033[32m *** SUCCESS BUILD $dockerProfile/$prefix$1 ***"
 else
-  echo -e "\033[31m *** ERROR DUE BUILDING mausved/wave_$1 ***"
+  echo -e "\033[31m *** ERROR DUE BUILDING $dockerProfile/$prefix$1 ***"
 fi
 
 echo -e "\033[0m"
