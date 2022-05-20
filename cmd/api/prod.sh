e41e22335f45f79873bb1fe12210232d64462c0d
mausved <binmaldo@mail.ru>
2022-05-20 23:15:23 +0300

    WV-76: added generate docs for prod (maybe need to remove)

--- cmd/api/prod.sh
+++ cmd/api/prod.sh
@@ -3,5 +3,6 @@ go get -u google.golang.org/protobuf
 go get -u google.golang.org/grpc
 go get -u golang.org/x/net/context
 bash gRPC.sh
+swag init -g cmd/api/main.go
 go build -o ./tmp/wave.music ./cmd/api/main.go
 
