5360635a37aa0aebb00750a08994cf048dbcd07d
mausved <binmaldo@mail.ru>
2022-05-20 22:46:53 +0300

    WV-76: fix demon

--- deploy/dockerBuild.sh
+++ deploy/dockerBuild.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 source spin.sh
 
-#dockerProfile="mausved"
-#prefix="wave_"
+dockerProfile="mausved"
+prefix="wave_"
 
 cd ..
 
-echo -e "\033[32m *** BUILDING mausved/wave_$1 ***\033[36m"
-docker build -q -t "$dockerProfile/$prefix$1" -f env/prod/k8/$1/Dockerfile . &
-
+echo -e "\033[32m *** BUILDING $dockerProfile/$prefix$1 ***\033[36m"
+docker build -q -t "$dockerProfile/$prefix$1" -f env/prod/k8/$1/Dockerfile .
 
 #spin
 
--- deploy/dockerPush.sh
+++ deploy/dockerPush.sh
@@ -5,16 +5,16 @@ dockerProfile="mausved"
 prefix="wave_"
 
 echo -e "\033[32m *** PUSHING $dockerProfile/$prefix$1 ***\033[36m"
-docker push -q "$dockerProfile/$prefix$1" &
+docker push -q "$dockerProfile/$prefix$1"
 
 
 #spin
 
 if [ $? -eq 0 ]
 then
-  echo -e "\033[32m *** SUCCESS PUSHED mausved/wave_$1 ***"
+  echo -e "\033[32m *** SUCCESS PUSHED $dockerProfile/$prefix$1 ***"
 else
-  echo -e "\033[31m *** ERROR DUE PUSHING mausved/wave_$1 ***"
+  echo -e "\033[31m *** ERROR DUE PUSHING $dockerProfile/$prefix$1 ***"
 fi
 
 echo -e "\033[0m"
