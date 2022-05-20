43828897abbfbf30be71998da9b0e133eca6e83d
mausved <binmaldo@mail.ru>
2022-05-20 23:33:05 +0300

    WV-76: corrected executable name

--- cmd/api/prod.sh
+++ cmd/api/prod.sh
@@ -4,5 +4,5 @@ go get -u google.golang.org/grpc
 go get -u golang.org/x/net/context
 bash gRPC.sh
 swag init -g cmd/api/main.go
-go build -o ./tmp/wave.music ./cmd/api/main.go
+go build -o ./tmp/api ./cmd/api/main.go
 
