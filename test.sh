990429b40961a0782048bf8a1d47b7c11c537dfc
mausved <binmaldo@mail.ru>
2022-05-22 16:26:55 +0300

    WV-76: remove "easyjson" pattern from test cover

--- test.sh
+++ test.sh
@@ -5,9 +5,10 @@ bash mocks.sh
 cd ../..
 
 go list ./...
-CVPKG=$(go list ./... | grep -Ev 'mocks|*.(P|p)roto' | tr '\n' ',')
+#CVPKG=$(go list ./... | grep -Ev 'mocks|*.(P|p)roto' | tr '\n' ',')
 echo -e "\033[32m go tests\033[0m"
-go test -coverpkg "$CVPKG" -coverprofile cover.out ./...
+go test . -coverprofile cover.out.tmp ./...
+cat cover.out.tmp | grep -v -Ev ".*mocks.*|.*.(P|p)roto.*|.*easyjson.*|.*_generated.go" > cover.out
 go tool cover -func cover.out | grep total
 if [ $? -eq 0 ]; then
   echo -e "\033[32m SUCCESS\033[0m"
