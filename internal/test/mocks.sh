c8787919afea601981b730378405891324cdf492
mausved <binmaldo@mail.ru>
2022-05-22 14:25:46 +0300

    WV-76: added exit code from mocks generate func and simplified bool comparison

--- internal/test/mocks.sh
+++ internal/test/mocks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo -e "\033[32m GenerateMocks"
-echo -e "\033[0m"
+echo -e "\033[32m GenerateMocks\033[0m"
 mockery --name=".*(Agent|Repo|UseCase$)" --dir="../" -r
-echo -e "\033[32m Success\n"
-echo -e "\033[0m"
+if [ $? -eq 0 ]; then
+  echo -e "\033[32m SUCCESS GENERATE MOCKS \n\033[0m\033[0m"
+else
+    echo -e "\033[31m *** ERROR DUE GENERATE MOCKS ***\033[0m"
+    exit 127
+fi
