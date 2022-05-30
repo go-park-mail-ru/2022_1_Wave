6d836ea98e47454b3a8b1131946122c24e554e52
mausved <binmaldo@mail.ru>
2022-05-30 18:53:16 +0300

    WV-80: added script for pulling

---
+++ deploy/dockerPullAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash dockerPullAll.sh
+if [ $? -eq 0 ]; then
+  echo -e "\033[32m *** SUCCESS PULLED ALL  ***\033[0m"
+else
+  echo -e "\033[31m *** ERROR DUE PULLING ALL ***\033[0m"
+  exit 127
+fi
